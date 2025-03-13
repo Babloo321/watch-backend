63b07d8a89ca1085a4c5dc251f62bd237e6644fe
Babloo Kumar <97720510+Babloo321@users.noreply.github.com>
2025-03-13 15:22:42 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -4,18 +4,23 @@ import cookieParser from "cookie-parser";
 const app = express();
 // cors policy for client
 const allowedOrigins = ["http://localhost:3000","*"];
+// app.use(cors({
+//   origin: function (origin, callback) {
+//     if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error("Not allowed by CORS"));
+//     }
+//   },
+//   credentials: true,
+//   methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+//   allowedHeaders: ["Content-Type", "Authorization"]
+// }));
 app.use(cors({
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
-  allowedHeaders: ["Content-Type", "Authorization"]
+  origin: "*",
+  allowedHeaders: "Content-Type,Authorization"
 }));
+
 
 app.use(express.json({limit:"16kb"}));
 app.use(express.urlencoded({limit:"16kb",extended:true}));  // when data comes to the url, it will be in json format,extended makes it nested(you can send nested data in url)
